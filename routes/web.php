851cae6885d637ffb3b00802a1ac8403d1aa39ab
Kike <5785235@alu.murciaeduca.es>
2026-02-18 08:38:31 +0100

    Mostrando mensajes flash de sesión

--- routes/web.php
+++ routes/web.php
@@ -132,7 +132,7 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth'])->group(function () {
     // Formulario de importación
     Route::get('/portfolio/import', [PortfolioImportController::class, 'showImportForm'])
-        ->name('portfolio.import.index');
+        ->name('portfolio.import.form');
 
     // Importar desde JSON Resume
     Route::post('/portfolio/import/json-resume', [PortfolioImportController::class, 'importJsonResume'])
